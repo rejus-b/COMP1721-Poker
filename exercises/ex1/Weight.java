9cb575f490260502ca1c6da1fd41a33418a388aa
Rejus Bulevicius <ed20rb5@leeds.ac.uk>
2022-01-27 15:05:41 +0000

    Finished code w/ comments

--- exercises/ex1/Weight.java
+++ exercises/ex1/Weight.java
@@ -1,14 +1,22 @@
 import java.util.Scanner;
 
 class Weight {
-      public static void main(String[] args) {
+    public static void main(String[] args) {
+        
+        int pounds;
+        double ounces, kgWeight;
 
-
+        // For getting the user input
         System.out.print("Enter weight in kilograms: ");
         Scanner userInput = new Scanner(System.in);
-        double kgWeight = userInput.nextInt();
-        
+        kgWeight = userInput.nextDouble();
 
-        System.out.printf("Equivalent imperial weight is %.1f lb %.1f oz", kgWeight, kgWeight);
+        // Converting kg to lbs and oz
+        ounces = kgWeight * 35.27;
+        pounds = (int) (ounces / 16); 
+        ounces = ounces % 16;
+
+        // %d is formatting for decimal integer
+        System.out.printf("Equivalent imperial weight is %d lb %.1f oz", pounds, ounces);
     }
 }
