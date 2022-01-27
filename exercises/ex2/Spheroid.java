3d1728cba3263447142662f9fea3d92d49b3b2b1
Rejus Bulevicius <ed20rb5@leeds.ac.uk>
2022-01-27 16:29:05 +0000

    Inputs working

--- exercises/ex2/Spheroid.java
+++ exercises/ex2/Spheroid.java
@@ -3,7 +3,21 @@ import java.util.Scanner;
 class Spheroid {
     public static void main(String[] args){
 
+        double eqRadius, poRadius, eccentricity, volume;
+        double pi = Math.PI;
 
+        // Scanning for equitorial radius
+        System.out.print("Enter equitorial radius in km: ");
+        Scanner userInput = new Scanner(System.in);
+        eqRadius = userInput.nextDouble();
 
+        // Scanning for polar radius
+        System.out.print("Enter polar radius in km: ");
+        userInput = new Scanner(System.in);
+        poRadius = userInput.nextDouble();
+
+        
+        //Math.pow(num, power);
+        
     }
 }
