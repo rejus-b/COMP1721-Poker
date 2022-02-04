30c31d07964868565a230573d81233742d61db13
Rejus Bulevicius <ed20rb5@leeds.ac.uk>
2022-02-04 00:59:04 +0000

    Using .err instead out .out for error msgs

--- exercises/ex4/CheckPassword.java
+++ exercises/ex4/CheckPassword.java
@@ -32,7 +32,7 @@ public class CheckPassword {
   public static void main(String[] args) {
 
     if (args.length == 0){
-      System.out.println("Usage: java CheckPassword <password>");
+      System.err.println("Usage: java CheckPassword <password>");
       System.exit(1);
     }
 
