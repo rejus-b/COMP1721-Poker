a400b8709bb5e8d8d7f495498e79470d06293628
Rejus Bulevicius <ed20rb5@leeds.ac.uk>
2022-02-13 05:10:06 +0000

    meanValue method finished, Main fixed errors

--- exercises/ex5/MeanValue.java
+++ exercises/ex5/MeanValue.java
@@ -1,5 +1,14 @@
 public class MeanValue {
   public static double meanValue(double[] data) {
+  
+    double total = 0;
+    int i; // counter
+    for (i = 0; i < data.length; i++){
+      total = total + data[i];
+    }
+
+    return(total/data.length);
+
   }
 
   public static void main(String[] args) {
@@ -10,12 +19,13 @@ public class MeanValue {
     }
 
     int i; // counter
-    double doubleArgs = [];
-    for (i = 0; i < args.length - 1; i++){
+    double [] doubleArgs = new double[args.length];
+    for (i = 0; i < args.length; i++){
       doubleArgs[i] = Double.parseDouble(args[i]);
+      System.out.println(doubleArgs[i]);
     }
 
-    System.out.printf(MeanValue[doubleArgs]);
+    System.out.printf("Mean value = %.3f", meanValue(doubleArgs));
 
   }
 }
