74c94decb0c04a437a34aa353ea8a04f21962226
Rejus Bulevicius <ed20rb5@leeds.ac.uk>
2022-02-15 18:17:47 +0000

    Circle.java first cycle

--- exercises/ex6/Circle.java
+++ exercises/ex6/Circle.java
@@ -1,6 +1,7 @@
 public class Circle {
 
     private double radius;
+    static double PI = Math.PI; 
 
     public Circle(double r) {
         radius = r;
@@ -8,14 +9,22 @@ public class Circle {
 
     public double getRadius(){
 
+        return radius;
+
     }
 
     public double area(){
+
+        // Area = PI * r^2
+        return(PI*(Math.pow(radius,2)));
 
     }
 
     public double perimiter(){
         
+        // Permiter = 2 * PI * r
+        return (2*PI*radius);
+
     }
 
 }
