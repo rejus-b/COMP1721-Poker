1d8681f84f5f7bab0778bdb922f4a9b0c599bb96
Rejus Bulevicius <ed20rb5@leeds.ac.uk>
2022-02-15 17:03:41 +0000

    Seketon UML

---
+++ exercises/ex6/Circle.java
@@ -0,0 +1,21 @@
+public class Circle {
+
+    private double radius;
+
+    public Circle(double r) {
+        radius = r;
+    }
+
+    public double getRadius(){
+
+    }
+
+    public double area(){
+
+    }
+
+    public double perimiter(){
+        
+    }
+
+}
