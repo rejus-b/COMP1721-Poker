d408f03ddc6b81ec8c18862bbe39c7828c18c52e
Rejus Bulevicius <ed20rb5@leeds.ac.uk>
2022-02-27 19:54:27 +0000

    Skeleton UML Working

--- exercises/ex9/AlarmClock.java
+++ exercises/ex9/AlarmClock.java
@@ -1,18 +1,18 @@
-public class AlaramClock extends Clock{
+public class AlarmClock extends Clock{
 
     private int alarmHours;
     private int alarmMinutes;
 
     public AlarmClock(){
-        super.Clock(0,0,0);
+        super(0,0,0);
     }
 
     public AlarmClock(int h, int m){
-        super.Clock(h,m,0);
+        super(h,m,0);
     }
 
     public AlarmClock(int h, int m, int s){
-        super.Clock(h,m,s);
+        super(h,m,s);
     }
 
     public int getAlarmHours(){
