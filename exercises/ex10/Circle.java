cffb3e41e061a16f7e77830c5cf090addd2ff54c
Rejus Bulevicius <ed20rb5@leeds.ac.uk>
2022-02-28 17:06:54 +0000

    Writeable interface skeleton code

---
+++ exercises/ex10/Circle.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.lang.Math;
+import java.io.FileWriter;
+import java.io.Writer;
+
+public class Circle {
+
+    private double radius;
+    //static double PI = Math.PI; 
+
+    public Circle(double r) {
+        radius = r;
+    }
+
+    public double getRadius(){
+
+        return radius;
+
+    }
+
+    public double area(){
+
+        // Area = PI * r^2
+        double circleArea;
+        circleArea = Math.PI*(radius*radius);
+        return(circleArea);
+
+    }
+
+    public double perimeter(){
+        
+        // Permiter = 2 * PI * r
+        double doublePerimeter;
+        doublePerimeter = 2*Math.PI*radius;
+        return (doublePerimeter);
+
+    }
+
+    public void writeTo(Writer destination){
+
+    }
+
+}
---
+++ exercises/ex10/Writeable.java
@@ -0,0 +1,7 @@
+import java.io.FileWriter;
+import java.io.Writer;
+
+interface Writeable{
+
+    Writer WriteTo();
+}
