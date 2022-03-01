aa28f8dc7989b4f163e16eaf2b4e29cf14b80ff9
Rejus Bulevicius <ed20rb5@leeds.ac.uk>
2022-03-01 14:26:53 +0000

    Working test cases

--- coursework/cwk1/src/main/java/comp1721/cwk1/WordList.java
+++ coursework/cwk1/src/main/java/comp1721/cwk1/WordList.java
@@ -3,27 +3,43 @@ package comp1721.cwk1;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 
 public class WordList {
 
   private List<String> words = new ArrayList<>();
 
   // TODO: Implement constructor with a String parameter
-  WordList(String filename){
+  public WordList(String filename) throws IOException{
+
+    try (Scanner input = new Scanner(Paths.get(filename))) {
+
+        while (input.hasNextLine()){
+            String value = input.nextLine();                
+            words.add(value);
+        }
+
+        input.close();
+    }
 
   }
 
 
   // TODO: Implement size() method, returning an int
-  int size(){
-
-    return(1);
+  public int size(){
+    return(words.size());
   }
 
 
   // TODO: Implement getWord() with an int parameter, returning a String
-  String getWord(int n){
-    
-    return("");
+  public String getWord(int n) throws GameException{ // gameNumber is the total amount of possible games e.g. 0 to the max number of words.
+    if (n < 0 || n > 4){
+      throw new GameException("Invalid game number.");
+    } else{
+      return(words.get(n));
+    }
   }
 }
