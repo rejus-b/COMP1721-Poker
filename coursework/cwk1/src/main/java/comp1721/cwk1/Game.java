b7081ac4720f0d3d3fd73e7783fe228abf2a5423
Rejus Bulevicius <ed20rb5@leeds.ac.uk>
2022-03-02 19:00:02 +0000

    Flushing changes

--- coursework/cwk1/src/main/java/comp1721/cwk1/Game.java
+++ coursework/cwk1/src/main/java/comp1721/cwk1/Game.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.time.*;
+import java.time.temporal.ChronoUnit;
+//import java.util.Date.*;
 
 // Import chromium for dates dont use the reecomended stuff its jank
 
@@ -17,22 +19,28 @@ public class Game {
     LocalDate today = LocalDate.now();
     LocalDate firstWordleDay = LocalDate.of(2021,Month.JUNE,19);
 
-    int daysDifference = ChronoUnit.DAYS.between(firstWordleDay, today);
-    gameNumber = daysDifference;
+    long daysDifference = ChronoUnit.DAYS.between(firstWordleDay, today);
+    gameNumber = (int) daysDifference;
 
-    target = WordList.getWord(gameNumber);
+    WordList words = new WordList(filename);
+    target = words.getWord(gameNumber);
 
   }
 
 
   // TODO: Implement constructor with int and String parameters
   public Game(int num, String filename) throws IOException{
+    gameNumber = num;
+
+    WordList words = new WordList(filename);
+    target = words.getWord(num);
 
   }
 
 
   // TODO: Implement play() method
   public void play(){
+    
 
   }
 
