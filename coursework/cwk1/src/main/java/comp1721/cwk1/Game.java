302aa9028d6205f224f393c931a55d559bdef941
Rejus Bulevicius <ed20rb5@leeds.ac.uk>
2022-03-02 20:47:55 +0000

    play() working

--- coursework/cwk1/src/main/java/comp1721/cwk1/Game.java
+++ coursework/cwk1/src/main/java/comp1721/cwk1/Game.java
@@ -13,6 +13,7 @@ public class Game {
 
   private int gameNumber;
   private String target; 
+  private List<String> guesses = new ArrayList<>();
 
   // TODO: Implement constructor with String parameter
   public Game(String filename) throws IOException{
@@ -40,9 +41,34 @@ public class Game {
 
   // TODO: Implement play() method
   public void play(){
-    
+      System.out.printf("Wordle %d %n%n", gameNumber);
 
-  }
+      for(int i = 1; i < 7; i++) {
+        Guess guess = new Guess(i);
+        System.out.println(target); // REMOVE THIS LATER // Original word for today was CHOKE
+        System.out.printf("Enter guess (%d/6): ", guess.getGuessNumber());
+        guess.readFromPlayer();
+        Guess validateGuess = new Guess(i, guess.getChosenWord());
+        System.out.println(guess.compareWith(target));
+        guesses.add(guess.compareWith(target));
+
+        if (guess.matches(target)){ // If its a first round guess
+          if (i == 1){
+            System.out.print("Superb - Got it in one!");
+            break;
+          } else if ((i > 1) && (i < 6)){ // 2-5 Guesses
+            System.out.print("Well done!");
+            break;
+          } else if (i == 6){ // Last round guess
+            System.out.print("That was a close call!");
+            break;
+          }
+        } else if (i > 6){ // Didnt guess correct
+          System.out.print("Nope - Better luck next time!");
+          break;
+        }
+      }
+    }
 
 
   // TODO: Implement save() method, with a String parameter
