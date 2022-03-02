4c04f16fd76f345ea03f153a02d876528e45114f
Rejus Bulevicius <ed20rb5@leeds.ac.uk>
2022-03-02 17:44:50 +0000

    First constructor

--- coursework/cwk1/src/main/java/comp1721/cwk1/Game.java
+++ coursework/cwk1/src/main/java/comp1721/cwk1/Game.java
@@ -3,6 +3,9 @@ package comp1721.cwk1;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.time.*;
+
+// Import chromium for dates dont use the reecomended stuff its jank
 
 public class Game {
 
@@ -10,25 +13,32 @@ public class Game {
   private String target; 
 
   // TODO: Implement constructor with String parameter
-  Game(String filename){
+  public Game(String filename) throws IOException{
+    LocalDate today = LocalDate.now();
+    LocalDate firstWordleDay = LocalDate.of(2021,Month.JUNE,19);
+
+    int daysDifference = ChronoUnit.DAYS.between(firstWordleDay, today);
+    gameNumber = daysDifference;
+
+    target = WordList.getWord(gameNumber);
 
   }
 
 
   // TODO: Implement constructor with int and String parameters
-  Game(int num, String filename){
+  public Game(int num, String filename) throws IOException{
 
   }
 
 
   // TODO: Implement play() method
-  void play(){
+  public void play(){
 
   }
 
 
   // TODO: Implement save() method, with a String parameter
-  void save(String filename){
+  public void save(String filename){
 
   }
 }
