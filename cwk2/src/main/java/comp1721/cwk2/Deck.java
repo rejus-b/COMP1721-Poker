2bc1583d509a0520ece4cf814b9062706b5d2342
Rejus Bulevicius <ed20rb5@leeds.ac.uk>
2022-05-19 21:58:35 +0100

    Stub code working, with pass/fail tests

--- cwk2/src/main/java/comp1721/cwk2/Deck.java
+++ cwk2/src/main/java/comp1721/cwk2/Deck.java
@@ -1,12 +1,54 @@
 package comp1721.cwk2;
+import comp1721.cwk2.Card.Rank;
+import comp1721.cwk2.Card.Suit;
 
 // Implement Deck class here
 
 public class Deck extends CardCollection
 {
-    public Deck()
+    public Deck() 
+    {
+
+        super();
+        
+        for (Suit suit : Suit.values())
+        {
+            for (Rank rank : Rank.values())
+            {
+                Card card = new Card(rank, suit);
+                add(card);
+            }
+        }
+    }
+
+    public int size()
+    {
+        return 0;
+    }
+    
+    public boolean isEmpty()
+    {
+        return false;
+    }
+
+    public boolean contains()
+    {
+        return false;
+    }
+
+    public void discard()
+    {
+        
+    }
+
+    public Card deal()
+    {
+        return cards.get(0);
+    }
+
+    public void shuffle()
     {
 
     }
-    
+
 }
--- cwk2/src/main/java/comp1721/cwk2/PokerHand.java
+++ cwk2/src/main/java/comp1721/cwk2/PokerHand.java
@@ -1,11 +1,84 @@
 package comp1721.cwk2;
+import comp1721.cwk2.Card.Rank;
+import comp1721.cwk2.Card.Suit;
+import java.util.ArrayList;
+// import java.utils.random;
 
 // Implement PokerHand class here
 
 public class PokerHand extends CardCollection
 {
+    protected static final int FULL_SIZE = 5;
+    private ArrayList<Card> pokerHand = new ArrayList<>();
+
     public PokerHand()
     {
-
+        // super();
+        ArrayList<Card> pokerHand = new ArrayList<>();
     }
+    
+    public PokerHand(String chosenCards)
+    {
+        // super();
+        // private char rank = chosenCards.substring(0,1);
+        // private char suit = chosenCards.substring(1); 
+        // Card card = new Card(rank, suit);
+        // pokerHand.add(card);
+    }
+
+    // // public String toString()
+    // // {
+    // //     return("NULL");
+    // // }
+
+    // // public int size()
+    // // {
+    // //     return 0;
+    // // }
+
+    // public void discard()
+    // {
+        
+    // }
+
+    public void discardTo(Deck deck)
+    {
+       
+    }  
+
+    public boolean isPair()
+    {
+        return false;
+    }
+
+    public boolean isTwoPairs()
+    {
+        return false;
+    }
+
+    public boolean isThreeOfAKind()
+    {
+        return false;
+    }
+
+    public boolean isFourOfAKind()
+    {
+        return false;
+    }
+
+    public boolean isFullHouse()
+    {
+        return false;
+    }
+
+    public boolean isFlush()
+    {
+        return false;
+    }
+
+    public boolean isStraight()
+    {
+        return false;
+    }
+
 } 
