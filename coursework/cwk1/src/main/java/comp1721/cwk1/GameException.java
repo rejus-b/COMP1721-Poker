18dac86f89c498b0d1841f3cdb14e91ae9113e6a
Rejus Bulevicius <116126554+rejus-b@users.noreply.github.com>
2022-10-22 00:51:06 +0100

    Delete coursework/cwk1 directory

--- coursework/cwk1/src/main/java/comp1721/cwk1/GameException.java
+++
@@ -1,9 +0,0 @@
-// Exception class for use in COMP1721 Coursework 1
-
-package comp1721.cwk1;
-
-public class GameException extends RuntimeException {
-  public GameException(String message) {
-    super(message);
-  }
-}
